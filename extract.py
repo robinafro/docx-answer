92c444c2e47ea7ae435ac15bc8959373424aac00
Actulurus <actulurus@gmail.com>
2024-03-04 10:18:54 -0500

    extracting

--- extract.py
+++ extract.py
@@ -5,6 +5,18 @@ import os
 temp_folder = os.path.join(os.path.dirname("__file__"), "temp")
 
 def extract_text(pdf_filename):
+    result_text = []
+
     path = os.path.join(temp_folder, pdf_filename)
 
-    
+    for page_png in convert_from_path(path):
+        text = pytesseract.image_to_string(page_png, lang='eng')
+        print(text)
+
+        result_text.append(text)
+
+    return result_text
+
+if __name__ == "__main__":
+    pdf_filename = input("Enter filename: ")
+    extract_text(pdf_filename)
