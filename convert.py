422aefb5556650536ac8add1f4f6b4fa4c5ee851
Actulurus <actulurus@gmail.com>
2024-03-05 16:39:15 +0100

    Replace underscores with 'Fill here' text for the chatbot

--- convert.py
+++ convert.py
@@ -2,11 +2,21 @@ import sys
 import subprocess
 import re
 import os
+from docx import Document
 
 out_folder = os.path.join(os.path.dirname(__file__), "temp")
 
 def convert_to(source, timeout=None):
-    args = [libreoffice_exec(), '--headless', '--convert-to', 'pdf', '--outdir', out_folder, source]
+    temp_filename = os.path.join(os.path.join(os.path.dirname(__file__), "temp"), os.path.basename(source))
+    newdoc = Document(source)
+
+    for paragraph in newdoc.paragraphs:
+        for run in paragraph.runs:
+            run.text = re.sub(r"_+", "[ Doplň zde ]", run.text)
+
+    newdoc.save(temp_filename)
+
+    args = [libreoffice_exec(), '--headless', '--convert-to', 'pdf', '--outdir', out_folder, temp_filename]
 
     process = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=timeout)
     filename = re.search('-> (.*?) using filter', process.stdout.decode())
